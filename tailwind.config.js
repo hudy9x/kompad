c5537de90ed9fd3601de95379aca9a99f2ee5cd5
hudy9x <hudy9x@gmail.com>
2022-05-26 17:55:53 +0700

    refactor(darkmode): tell tailwindcss parse css inside containers folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./containers/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {},
