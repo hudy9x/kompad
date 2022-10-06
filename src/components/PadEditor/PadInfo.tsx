165929ec844d7982ceaca53c70abc4e372db8812
unknown <nam664221998@gmail.com>
2022-10-06 22:31:32 +0700

    fix: title pad is hidden

--- src/components/PadEditor/PadInfo.tsx
+++ src/components/PadEditor/PadInfo.tsx
@@ -13,42 +13,43 @@ interface IPadInfoContentProps {
 }
 
 function PadInfoContent({ info }: IPadInfoContentProps) {
-  const inpRef = useRef<HTMLInputElement>(null);
+  const title = useRef<string>();
   const { id } = useParams();
 
-  const updateTitle = () => {
+  const updateTitle = (e:any) => {
     if (timeout) {
       clearTimeout(timeout);
     }
 
     timeout = setTimeout(() => {
       // updatePad
-      if (!inpRef.current || !id) {
+      if (!title.current || !id) {
         return;
       }
-
-      updatePadMetadata({ id, title: inpRef.current.value });
+      
+      updatePadMetadata({ id, title: e.target.innerText });
     }, 500) as unknown as number;
   };
 
   useEffect(() => {
-    if (inpRef.current && info && info.title) {
-      inpRef.current.value = info.title;
-    }
-  }, [info]);
-
+    title.current = info.title;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [info.id]);
   const created = dayjs(info.createdAt.toDate()).format("YYYY/MM/DD");
 
   return (
     <div className="pad-infos">
-      <input
-        ref={inpRef}
-        // value={info.title}
-        onChange={updateTitle}
-        className="mb-5 h-12 md:w-[700px] xl:w-[800px] m-auto text-4xl text-gray-700 font-extrabold outline-none dark:bg-transparent dark:text-gray-100"
+      <p
+        key="editor1"  
+        onInput={updateTitle}
+        contentEditable={true}
+        className="inline-block mb-5 h-12 md:w-[700px] xl:w-[800px] m-auto text-4xl text-gray-700 font-extrabold outline-none dark:bg-transparent dark:text-gray-100"
         placeholder="Untitled"
-      />
-      <div className="pad-details space-y-2 text-gray-600">
+        suppressContentEditableWarning={true}
+      >
+        {title.current}
+      </p>
+      <div className="pad-details space-y-2 text-gray-600 mt-2">
         <div className="flex items-center text-sm">
           <span className="text-gray-400 w-32">Created at:</span>
           <span>{created}</span>
