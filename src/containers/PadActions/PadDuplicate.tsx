f096d883b40163646a21ab32e00cbac3e5ff5309
unknown <nam664221998@gmail.com>
2022-12-24 16:34:10 +0700

    feat: pad duplication

--- src/containers/PadActions/PadDuplicate.tsx
+++ src/containers/PadActions/PadDuplicate.tsx
@@ -1,8 +1,20 @@
 import { HiOutlineDocumentDuplicate } from 'react-icons/hi'
+import { message } from '../../components/message';
+import { IPad, duplicatePad } from '../../services/pads';
 
-export const PadDuplicate = () => {
+export const PadDuplicate = ({ data }: { data: IPad }) => {
+
+  const onDuplicate = async () => {
+    await duplicatePad(data.id!).then(() => {
+      message.success('Duplicated successfully');
+    }).catch(() => {
+      message.error('Duplicated failed');
+    });
+  }
+
   return (
     <a
+      onClick={onDuplicate}
       href="#duplicate"
       className="dropdown-content"
     >
