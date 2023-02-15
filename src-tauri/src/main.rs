628553ff4e533dcb4062f0503172ea3eccb34c48
nguyennam <94043947+nam999-FE@users.noreply.github.com>
2023-02-15 16:26:36 +0700

    fix: only set shadow for windows and macos platform, ignore linux (#98)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,3 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
-
 use tauri::Manager;
 use window_shadows::set_shadow;
 
@@ -15,11 +10,14 @@ fn main() {
             tauri::async_runtime::spawn(async move {
                 // initialize your app here instead of sleeping :)
                 println!("Initializing...");
-                // std::thread::sleep(std::time::Duration::from_millis(700));
                 std::thread::sleep(std::time::Duration::from_secs(2));
                 println!("Done initializing.");
 
-                set_shadow(&main_window, true).expect("Unsupported platform!");
+                // set_shadow is only supported on Windows and macOS
+                #[cfg(any(target_os = "windows", target_os = "macos"))]
+                {
+                    set_shadow(&main_window, true).expect("Unsupported platform!");
+                }
 
                 // After it's done, close the splashscreen and display the main window
                 splashscreen_window.close().unwrap();
