7df786e601c1de779b1f58d722a3e2b539be15bd
hudy9x <hudy9x@gmail.com>
2023-03-11 21:41:29 +0700

    fix: empty screen displayed due to app trying to getting secret key when user sign out

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ import Signup from "./containers/Signup";
 import ThemeColor from "./containers/Theme";
 import ThemeSetting from "./containers/Theme/ThemeSetting";
 import { isDesktopApp } from "./libs/utils";
-import AppMiddleware from "./providers/AppMiddleware";
 import { AuthenProvider } from "./providers/Authenticator";
 
 const lz = React.lazy;
@@ -36,52 +35,50 @@ function App() {
   return (
     <div className={`App ${isWebversion ? "is-web-app" : ""}`}>
       <AuthenProvider>
-        <AppMiddleware>
-          <ThemeSetting>
-            <Routes>
-              <Route path="/" element={<LayoutClear />}>
-                <Route index element={<Checking />} />
-                <Route path="signin" element={<Signin />} />
-                <Route path="signout" element={<Signout />} />
-                <Route path="signup" element={<Signup />} />
-                <Route path="forgot-password" element={<ForgotPassword />} />
-                <Route path="email-verification" element={<EmailVerification />} />
-              </Route>
-              <Route path="/app" element={<Layout />}>
-                <Route
-                  path="pad"
-                  element={
-                    <PrivateRoute>
-                      <Pad />
-                    </PrivateRoute>
-                  }
-                >
-                  <Route index element={<PadEmpty />}></Route>
-                  <Route path=":id" element={<PadContent />}></Route>
-                </Route>
-              </Route>
+        <ThemeSetting>
+          <Routes>
+            <Route path="/" element={<LayoutClear />}>
+              <Route index element={<Checking />} />
+              <Route path="signin" element={<Signin />} />
+              <Route path="signout" element={<Signout />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="email-verification" element={<EmailVerification />} />
+            </Route>
+            <Route path="/app" element={<Layout />}>
               <Route
-                path="setting"
+                path="pad"
                 element={
                   <PrivateRoute>
-                    <React.Suspense fallback={<>...</>}>
-                      <LayoutSetting />
-                    </React.Suspense>
+                    <Pad />
                   </PrivateRoute>
                 }
               >
-                {/* <Route index element={<PadEmpty />}></Route> */}
-                <Route path="profile" element={<Profile />}></Route>
-                <Route path="password" element={<Password />}></Route>
-                <Route path="plan" element={<Plan />}></Route>
-                <Route path="theme" element={<ThemeColor />}></Route>
-                <Route path="file-manager" element={<FileManager />}></Route>
-                <Route path="*" element={<NotFound />}></Route>
+                <Route index element={<PadEmpty />}></Route>
+                <Route path=":id" element={<PadContent />}></Route>
               </Route>
+            </Route>
+            <Route
+              path="setting"
+              element={
+                <PrivateRoute>
+                  <React.Suspense fallback={<>...</>}>
+                    <LayoutSetting />
+                  </React.Suspense>
+                </PrivateRoute>
+              }
+            >
+              {/* <Route index element={<PadEmpty />}></Route> */}
+              <Route path="profile" element={<Profile />}></Route>
+              <Route path="password" element={<Password />}></Route>
+              <Route path="plan" element={<Plan />}></Route>
+              <Route path="theme" element={<ThemeColor />}></Route>
+              <Route path="file-manager" element={<FileManager />}></Route>
               <Route path="*" element={<NotFound />}></Route>
-            </Routes>
-          </ThemeSetting>
-        </AppMiddleware>
+            </Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </ThemeSetting>
       </AuthenProvider>
     </div>
   );
