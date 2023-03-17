41ab2adaceb029383b3761c057d1fc1314cd729b
hudy9x <hudy9x@gmail.com>
2023-03-17 15:16:39 +0700

    fix: only show a full document list in mobile view

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -1,6 +1,7 @@
 import create from "zustand"
 import produce from "immer"
 import { getCache } from "../libs/localCache"
+import { isDesktopApp } from "../libs/utils"
 
 export interface ISettingStore {
   secondSidebarVisibility: boolean
@@ -18,6 +19,15 @@ const sidebar = getCache("SETTING_VIEW_SIDEBAR") === "0" ? false : true
 export const useSettingStore = create<ISettingStore>((set) => ({
   secondSidebarVisibility: false,
   toggleSecondSidebar: () => {
+    if (isDesktopApp()) {
+      return;
+    }
+
+    // ignore if the view is not mobile viewport
+    if (document.body.offsetWidth > 900) {
+      return;
+    }
+
     set(
       produce<ISettingStore>((state) => {
         state.secondSidebarVisibility = !state.secondSidebarVisibility
