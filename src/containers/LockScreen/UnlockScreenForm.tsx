f0fabc06b078ce297455c6c8a91f1ef2a4deb884
hudy9x <hudy9x@gmail.com>
2023-04-11 00:54:31 +0700

    convert attribute to JSX attribute

--- src/containers/LockScreen/UnlockScreenForm.tsx
+++ src/containers/LockScreen/UnlockScreenForm.tsx
@@ -85,7 +85,7 @@ export default function UnlockScreenForm({
           cy="12"
           r="10"
           stroke="currentColor"
-          stroke-width="4"
+          strokeWidth="4"
         ></circle>
         <path
           className="opacity-75"
