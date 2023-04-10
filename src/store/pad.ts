3b6a1f9f8355836a44df744a76060ba054f1930e
hudy9x <hudy9x@gmail.com>
2023-04-11 00:53:13 +0700

    code formatting

--- src/store/pad.ts
+++ src/store/pad.ts
@@ -1,24 +1,24 @@
-import create from "zustand";
-import produce from "immer";
-import { IPad } from "../services/pads";
+import { create } from "zustand"
+import produce from "immer"
+import { IPad } from "../services/pads"
 
 export interface IPadQuery {
-  tag: string;
-  folder: string;
-  recently?: boolean;
-  important?: boolean;
+  tag: string
+  folder: string
+  recently?: boolean
+  important?: boolean
 }
 
 export interface IPadStore {
-  pads: IPad[];
-  query: IPadQuery;
-  clearFilter: () => void;
-  filterByAll: (query: IPadQuery) => void;
-  filterByTag: (id: string) => void;
-  filterByFolder: (id: string) => void;
-  filterByRecently: () => void;
-  updatePadList: (data: IPad[]) => void;
-  filterByImportant: () => void;
+  pads: IPad[]
+  query: IPadQuery
+  clearFilter: () => void
+  filterByAll: (query: IPadQuery) => void
+  filterByTag: (id: string) => void
+  filterByFolder: (id: string) => void
+  filterByRecently: () => void
+  updatePadList: (data: IPad[]) => void
+  filterByImportant: () => void
 }
 
 // configure store
@@ -38,52 +38,54 @@ export const usePadListStore = create<IPadStore>((set) => ({
           tag: "",
           folder: "",
           recently: false,
-          important: false
-        };
+          important: false,
+        }
       })
     ),
 
   filterByTag: (id: string) =>
     set(
       produce<IPadStore>((state) => {
-        state.query.tag = id;
+        state.query.tag = id
       })
     ),
 
   filterByFolder: (id: string) =>
     set(
       produce<IPadStore>((state) => {
-        state.query.folder = id;
+        state.query.folder = id
       })
     ),
 
   filterByRecently: () => {
-    set(produce<IPadStore>(state => {
-      state.query.important = false;
-      state.query.recently = !state.query.recently
-    }))
+    set(
+      produce<IPadStore>((state) => {
+        state.query.important = false
+        state.query.recently = !state.query.recently
+      })
+    )
   },
 
   filterByAll: (query: IPadQuery) =>
     set(
       produce<IPadStore>((state) => {
-        state.query = query;
+        state.query = query
       })
     ),
 
   updatePadList: (data: IPad[]) =>
     set(
       produce<IPadStore>((state) => {
-        state.pads = data;
+        state.pads = data
       })
     ),
 
   filterByImportant: () => {
     set(
       produce<IPadStore>((state) => {
-        state.query.recently = false;
-        state.query.important = !state.query.important;
+        state.query.recently = false
+        state.query.important = !state.query.important
       })
-    );
+    )
   },
-}));
+}))
--- src/store/themes.ts
+++ src/store/themes.ts
@@ -1,4 +1,4 @@
-import create from "zustand"
+import { create } from "zustand"
 import { IUserThemeSettings } from "../services/user-settings"
 
 export interface IThemeStore {
