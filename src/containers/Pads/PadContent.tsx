334bc2bfce16d06d42fc643ca2724eb33b6493e0
hudy9x <hudy9x@gmail.com>
2023-04-12 15:35:12 +0700

    add caching for pad content

--- src/containers/Pads/PadContent.tsx
+++ src/containers/Pads/PadContent.tsx
@@ -1,46 +1,69 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import PadEditor from "../../components/PadEditor";
-import { decryptText } from "../../services/encryption";
-import { getPadById, IPad, saveCurrentPad } from "../../services/pads";
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import PadEditor from "../../components/PadEditor"
+import {
+  CURRENT_PAD_CONTENT,
+  getCacheJSON,
+  setCacheJSON,
+} from "../../libs/localCache"
+import { decryptText } from "../../services/encryption"
+import { getPadById, IPad, saveCurrentPad } from "../../services/pads"
 
 function PadContent() {
-  const { id } = useParams();
-  const [pad, setPad] = useState<IPad>();
+  // rendering pad's content at first
+  const cachedPad = getCacheJSON(CURRENT_PAD_CONTENT) as IPad
+  const { id } = useParams()
+  const [pad, setPad] = useState<IPad>(cachedPad)
 
   useEffect(() => {
     if (id) {
-      saveCurrentPad(id);
+      saveCurrentPad(id)
+
       getPadById(id).then((res) => {
-        if (!res) return;
+        if (!res) return
+
+        const data = {
+          content: res.content,
+          cipherContent: res.cipherContent,
+          createdAt: res.createdAt,
+          tags: res.tags,
+          title: res.title,
+          uid: res.uid,
+          updatedAt: res.updatedAt,
+          folder: res.folder || "",
+          cover: res.cover || "",
+          id: id,
+          shortDesc: res.shortDesc,
+          important: res.important,
+        }
+
+        if (
+          cachedPad &&
+          cachedPad.updatedAt &&
+          data.updatedAt.seconds <= cachedPad.updatedAt.seconds &&
+          cachedPad.id === id
+        ) {
+          console.log("no changes, do nothing")
+          return
+        }
+
+        setCacheJSON(CURRENT_PAD_CONTENT, data)
 
         setPad((prevPad) => ({
           ...prevPad,
-          ...{
-            content: res.content,
-            cipherContent: res.cipherContent,
-            createdAt: res.createdAt,
-            tags: res.tags,
-            title: res.title,
-            uid: res.uid,
-            updatedAt: res.updatedAt,
-            folder: res.folder || "",
-            cover: res.cover || "",
-            id: res.id,
-            shortDesc: res.shortDesc,
-            important: res.important,
-          },
-        }));
-      });
+          ...data,
+        }))
+      })
     }
-  }, [id]);
+    //eslint-disable-next-line
+  }, [id])
 
   const getContent = (pad: IPad) => {
     if (pad.cipherContent) {
-      return decryptText(pad.cipherContent);
+      return decryptText(pad.cipherContent)
     }
-    return pad.content;
-  };
+    return pad.content
+  }
 
   return (
     <>
@@ -48,7 +71,7 @@ function PadContent() {
         <PadEditor data={pad} id={id} content={getContent(pad)} />
       ) : null}
     </>
-  );
+  )
 }
 
-export default PadContent;
+export default PadContent
