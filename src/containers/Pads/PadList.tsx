66fb6ce801300cd153482696e2bea19eed95a8e4
hudy9x <hudy9x@gmail.com>
2023-04-25 11:14:31 +0700

    add total of notes

--- src/containers/Pads/PadList.tsx
+++ src/containers/Pads/PadList.tsx
@@ -15,11 +15,11 @@ dayjs.extend(relativeTime)
 function PadList() {
   const { user } = useAuth()
   const { id } = useParams()
-  const [loading, setLoading] = useState(false)
   const { pads, query, updatePadList, appendPads } = usePadListStore()
   const [lastDoc, setLastDoc] = useState<QueryDocumentSnapshot<unknown> | null>(
     null
   )
+  const [end, setEnd] = useState("NONE")
   const isFetching = useRef(false)
   const needToLoadMoreData = useRef(false)
 
@@ -28,22 +28,25 @@ function PadList() {
       return
     }
 
+    if (!lastDoc) {
+      setEnd("END")
+      return
+    }
+
     if (!user || !user?.uid || !lastDoc || isFetching.current) return
     const clonedQuery = { ...query }
 
     clonedQuery.startAfter = lastDoc
     isFetching.current = true
-    setLoading(true)
 
     getPadsByUidQuery(user.uid, clonedQuery)
       .then(({ lastDoc, data }) => {
-        console.log(lastDoc, data)
         appendPads(data)
         setLastDoc(lastDoc)
+        !lastDoc && setEnd("END")
       })
       .finally(() => {
         isFetching.current = false
-        setLoading(false)
       })
   }
 
@@ -56,6 +59,7 @@ function PadList() {
         }
 
         if (last) {
+          setEnd("NONE")
           setLastDoc(last)
         }
 
@@ -71,24 +75,29 @@ function PadList() {
   }, [user?.uid, query])
 
   return (
-    <ScrollBar
-      height="calc(100vh - 71px)"
-      onScrollStop={loadMore}
-      onScrollFrame={(values) => {
-        needToLoadMoreData.current = values.top > 0.9
-      }}
-    >
-      <div className="pad-list">
-        {pads.map((pad) => {
-          return (
-            <ContextMenu key={pad.id}>
-              <PadItem active={id === pad.id} pad={pad} />
-            </ContextMenu>
-          )
-        })}
+    <>
+      <ScrollBar
+        height="calc(100vh - 80px)"
+        onScrollStop={loadMore}
+        onScrollFrame={(values) => {
+          needToLoadMoreData.current = values.top > 0.9
+        }}
+      >
+        <div className="pad-list">
+          {pads.map((pad) => {
+            return (
+              <ContextMenu key={pad.id}>
+                <PadItem active={id === pad.id} pad={pad} />
+              </ContextMenu>
+            )
+          })}
+        </div>
+      </ScrollBar>
+      <div className="text-2xs px-4 border-t border-color-dark flex items-center justify-between uppercase">
+        <span>{end === "END" ? "Reached Limit" : ""}</span>
+        <span>Total: {pads.length}</span>
       </div>
-      <div>{loading ? "Loading" : <></>}</div>
-    </ScrollBar>
+    </>
   )
 }
 
