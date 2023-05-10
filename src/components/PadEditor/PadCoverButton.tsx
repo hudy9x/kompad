6fef12b3b3e27bf5f2109fc0eab1891beec003aa
hudy9x <hudy9x@gmail.com>
2023-05-10 19:53:33 +0700

    add code

--- src/components/PadEditor/PadCoverButton.tsx
+++ src/components/PadEditor/PadCoverButton.tsx
@@ -1,56 +1,87 @@
 import { Timestamp } from "firebase/firestore"
+import { useState } from "react"
 import { useAuth } from "../../hooks/useAuth"
-import { addFileInfo, deleteCoverImageFile, deleteFile, getFileUrl, uploadFileToPad } from "../../services/files"
+import {
+  addFileInfo,
+  deleteCoverImageFile,
+  deleteFile,
+  getFileUrl,
+  uploadFileToPad,
+} from "../../services/files"
 import { updatePadMetadata } from "../../services/pads"
+import Modal from "../Modal"
 
 interface Props {
   id: string
 }
 
 export default function PadCoverButton({ id }: Props) {
-  const { user } = useAuth();
+  const { user } = useAuth()
+  const [visible, setVisible] = useState(false)
 
   const onChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     const filePath = `pad-cover-${id}`
     const files = ev.target.files
 
-    if (!user) return;
+    if (!user) return
 
-    if (!files || !files.length) return;
+    if (!files || !files.length) return
 
     deleteCoverImageFile({
       createdBy: user.uid,
       padId: id,
-      source: 'COVER-IMAGE'
+      source: "COVER-IMAGE",
     }).then(() => {
-
       deleteFile(filePath).then(() => {
-        const file = files[0];
-        uploadFileToPad(filePath, file).then(getFileUrl).then(url => {
-          const path = `pads/${user.uid}/${filePath}`;
-          addFileInfo({
-            name: file.name,
-            size: file.size / 1024 / 1024,
-            url,
-            path: path,
-            type: file.type,
-            padId: id,
-            createdAt: Timestamp.now(),
-            createdBy: user.uid,
-            source: 'COVER-IMAGE',
+        const file = files[0]
+        uploadFileToPad(filePath, file)
+          .then(getFileUrl)
+          .then((url) => {
+            const path = `pads/${user.uid}/${filePath}`
+            addFileInfo({
+              name: file.name,
+              size: file.size / 1024 / 1024,
+              url,
+              path: path,
+              type: file.type,
+              padId: id,
+              createdAt: Timestamp.now(),
+              createdBy: user.uid,
+              source: "COVER-IMAGE",
+            })
+            updatePadMetadata({ id, cover: url }).then((res) =>
+              console.log(res)
+            )
           })
-          updatePadMetadata({ id, cover: url }).then(res => console.log(res))
-        })
       })
     })
-
   }
 
-  return <div className="pad-cover-upload absolute z-20 right-3 top-2 cursor-pointer shadow-md">
-    <label htmlFor="pad-inp-update-cover" style={{ fontSize: '0.625rem' }}
-      className="transition uppercase opacity-50 hover:opacity-100 cursor-pointer px-2 py-1 rounded-full">
-      Change cover</label>
-    <input onChange={onChange} id="pad-inp-update-cover" type="file" className="hidden" />
-  </div>
-
+  return (
+    <div className="pad-cover-upload absolute z-20 right-3 top-2 cursor-pointer shadow-md">
+      <button
+        className="btn btn-primary uppercase text-2xs"
+        onClick={() => {
+          setVisible(true)
+        }}
+      >
+        Change cover
+      </button>
+      <Modal visible={visible} setVisible={setVisible}>
+        <label
+          htmlFor="pad-inp-update-cover"
+          style={{ fontSize: "0.625rem" }}
+          className="transition uppercase opacity-50 hover:opacity-100 cursor-pointer px-2 py-1 rounded-md"
+        >
+          Change cover
+        </label>
+        <input
+          onChange={onChange}
+          id="pad-inp-update-cover"
+          type="file"
+          className="hidden"
+        />
+      </Modal>
+    </div>
+  )
 }
